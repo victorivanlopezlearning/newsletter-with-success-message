29a6b1171ab8d6a72ef4388d4eb26f80a2fa618d
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-02 21:44:55 -0600

    gulpfile.js: add SASS functionality

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,13 @@ const { src, dest } = require('gulp');
 // SASS
 const sass = require('gulp-sass')(require('sass'));
 
+// GULP
+const plumber = require('gulp-plumber');
+
 function css() {
     return src('src/scss/app.scss')
+        .pipe(plumber())
         .pipe( sass().on('error', sass.logError) )
         .pipe( dest('assets/css') )
 };
-
 exports.css = css;

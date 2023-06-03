106251c8ed10c42732f4137b94ee4ddbc58528ac
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-02 22:32:23 -0600

    gulpfile.js: add sourcemaps functionality

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,16 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 // Cssnano
 const cssnano = require('cssnano');
+// sourcemaps
+const sourcemaps = require('gulp-sourcemaps');
 
 function css() {
     return src('src/scss/app.scss')
         .pipe(plumber())
+        .pipe( sourcemaps.init() )
         .pipe( sass().on('error', sass.logError) )
         .pipe( postcss( [autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
         .pipe( dest('assets/css') )
 };
 exports.css = css;

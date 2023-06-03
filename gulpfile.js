bdd4b62135de4ae0e4c0abe6f85471a4e2d14933
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-02 22:42:08 -0600

    gulpfile.js: add JavaScript task

--- gulpfile.js
+++ gulpfile.js
@@ -10,20 +10,32 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 // sourcemaps
 const sourcemaps = require('gulp-sourcemaps');
+// js
+const terser = require('gulp-terser-js');
 
 function css() {
     return src('src/scss/app.scss')
-        .pipe(plumber())
         .pipe( sourcemaps.init() )
+        .pipe(plumber())
         .pipe( sass().on('error', sass.logError) )
         .pipe( postcss( [autoprefixer(), cssnano() ]) )
         .pipe(sourcemaps.write('.'))
         .pipe( dest('assets/css') )
 };
 
+function js() {
+    return src('src/js/**/*.js') 
+             .pipe( sourcemaps.init() ) 
+             .pipe( terser() )
+             .pipe( sourcemaps.write('.')) 
+             .pipe( dest('assets/js'));
+ };
+
 function dev() {
     watch( 'src/scss/**/*.scss', css);
+    watch( 'src/js/**/*.js', js);
 };
 
 exports.css = css;
+exports.js = js;
 exports.dev = dev;

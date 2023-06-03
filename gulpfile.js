141d2a2872c627314e2786e17ee712fa9f93c084
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-02 21:49:48 -0600

    gulpfile.js: add Autoprefixer functionality

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const { src, dest } = require('gulp');
 
 // SASS
 const sass = require('gulp-sass')(require('sass'));
-
-// GULP
 const plumber = require('gulp-plumber');
+// Autoprefixer
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css() {
     return src('src/scss/app.scss')
         .pipe(plumber())
         .pipe( sass().on('error', sass.logError) )
+        .pipe( postcss( [autoprefixer()] ) )
         .pipe( dest('assets/css') )
 };
 exports.css = css;
